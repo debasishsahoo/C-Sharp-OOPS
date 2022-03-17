a18aec8581e56be2c7e81682911d2a3423fee8c6
debasishsahoo <debasish.sahoo96@gmail.com>
2022-03-17 10:59:22 +0530

    Inher

---
+++ 7.AbstractionsDemo/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.AbstractionsDemo
+{
+    public class Laptop
+    {
+        private string _name = "HP";
+        private string _model = "i7";
+        private int _count = 2+2+8+6+7+91-40;
+        private void Price() {
+            Console.WriteLine("Price:$1000");
+        }
+
+        public string Name {
+        set { _name = value; }
+        get { return _name; }
+        }
+        public string Model {
+            set { _model = value; }
+            get { return _model; }
+
+        }
+
+        public int Count
+        {
+            set { _count = value; }
+            get { return _count; }
+
+        }
+
+        public void Color() {
+            Console.WriteLine("Color:BLACK");
+        }
+        public void Ram() {
+            Console.WriteLine("RAM:16GB");
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Laptop laptop = new Laptop();
+
+            string LaptopName=laptop.Name;
+            string LaptopModel = laptop.Model;
+            int LaptopCount = laptop.Count;
+
+            laptop.Color();
+            laptop.Ram();
+
+            //laptop._name;
+            //laptop.Price();
+
+
+        }
+    }
+}
---
+++ 8.Inheritance/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.Inheritance
+{
+    //Single Inheritance       base class ->derived class
+
+    //Multilevel Inheritance   base class -> derived class -> derived class
+
+    //Multiple Inheritance     (baseclass1 baseclass2) -> derived class
+
+    //Hiearchical Inheritance  (baseclass) -> (derived1 class,derived2 class)
+
+    //Hybrid Inheritance
+
+    #region Rules
+    //Rule 1:In Inheritance the constracor of the parent class must be accessible to its child class,else the Inheritance will not possible. because when we create the child class object first it goes and call the parent class constractor so that the parent class variable will be init and can comsume them under the child class.
+
+    //Rule 2:In Inheritance the child class consume the parent class members but the parent class not do so.
+
+    //Rule 3:class can be assigned to a variable of the same class to make it as a reference,ii can also be assigned a varible of its parant to make it as a reference so that the reference startconsuming memory of the object assigned ti it,but alsousing that we control access child class pure memeber
+  //a Parent class object can never be assigned to a child class variable.
+    #endregion
+
+
+    class Branch {
+        int BranchCode;
+        string BranchName, BranchAddress;
+        public void GetBranchData()
+        {
+            Console.WriteLine("ENTER BRANCH DETAILS:");
+            Console.WriteLine("ENTER BRANCH CODE");
+            BranchCode = int.Parse(Console.ReadLine());
+            Console.WriteLine("ENTER BRANCH NAME");
+            BranchName = Console.ReadLine();
+            Console.WriteLine("ENTER BRANCH ADDRESS");
+            BranchAddress = Console.ReadLine();
+        }
+        public void DisplayBranchData()
+        {
+            Console.WriteLine("BRANCH CODE IS : " + BranchCode);
+            Console.WriteLine("BRANCH NAME IS : " + BranchName);
+            Console.WriteLine("BRANCH ADDRESS IS : " + BranchAddress);
+        }
+    }
+    class Employee:Branch {
+        int EmployeeId, EmployeeAge;
+        string EmployeeName, EmployeeAddress;
+        public void GetEmployeeData()
+        {
+            Console.WriteLine("ENTER EMPLYEE DETAILS:");
+            Console.WriteLine("ENTER EMPLOYEE ID");
+            EmployeeId = int.Parse(Console.ReadLine());
+            Console.WriteLine("ENTER EMPLOYEE AGE");
+            EmployeeAge = int.Parse(Console.ReadLine());
+            Console.WriteLine("ENTER EMPLOYEE NAME");
+            EmployeeName = Console.ReadLine();
+            Console.WriteLine("ENTER EMPLOYEE ADDRESS");
+            EmployeeAddress = Console.ReadLine();
+        }
+        public void DisplayEmployeeData()
+        {
+            Console.WriteLine("EMPLOYEE ID IS : " + EmployeeId);
+            Console.WriteLine("EMPLOYEE NAME IS : " + EmployeeName);
+            Console.WriteLine("EMPLOYEE ADDRESS IS : " + EmployeeAddress);
+            Console.WriteLine("EMPLOYEE AGE IS : " + EmployeeAge);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee obj1 = new Employee();
+            obj1.GetBranchData();
+            obj1.GetEmployeeData();
+            obj1.DisplayBranchData();
+            obj1.DisplayEmployeeData();
+            Console.WriteLine("Press any key to exist.");
+            Console.ReadKey();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+}
