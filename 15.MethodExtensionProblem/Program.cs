4b7b4d088116154dab55e86104e8cd4ec0961810
debasishsahoo <debasish.sahoo96@gmail.com>
2022-03-28 12:11:03 +0530

    Exten

---
+++ 15.MethodExtensionProblem/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.MethodExtensionProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyWord = "C# is A Good Programing Language";
+            int WordCount= MyWord.GetWordCount();
+
+            Console.WriteLine("input String:"+ MyWord);
+            Console.WriteLine("WordCount:" + WordCount);
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
---
+++ 15.MethodExtensionProblem/StringExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.MethodExtensionProblem
+{
+    public static class StringExtension
+    {
+        public static int GetWordCount(this string inputsting)
+        {
+            if(!string.IsNullOrEmpty(inputsting))
+            {
+                string[] words = inputsting.Split(' ');
+                return words.Count();
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+    }
+}
